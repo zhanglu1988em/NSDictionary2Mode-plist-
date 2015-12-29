8fc85c17adba8389f378e72e06808eb7505c04e3
zhanglu1988em <zhanglu1988em@163.com>
2015-12-29 17:45:12 +0800

    Initial Commit

---
+++ NSDictionary2Mode_plist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSDictionary2Mode_plist
+//
+//  Created by wbder on 15/12/29.
+//  Copyright © 2015年 wbder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSDictionary2Mode_plist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSDictionary2Mode_plist
+//
+//  Created by wbder on 15/12/29.
+//  Copyright © 2015年 wbder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
